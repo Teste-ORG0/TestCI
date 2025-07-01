840fdb31005ec0adab14bd914563ff3bb14fd194
Matheus Barros <92684440+MatheusOB21@users.noreply.github.com>
2025-07-01 09:56:27 -0300

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,8 +1,11 @@
-number = rand(0..10)
+number = rand(0..100)
 
 if number > 7
   puts "❌ Falha!"
   exit(1)
+elsif number == 100
+  puts "✅ Você foi o grande vencedor!"
+  exit(0)
 elsif number < 4
   puts "✅ Falhou com sucesso!"
   exit(0)
